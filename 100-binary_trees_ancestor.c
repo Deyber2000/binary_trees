ba3cfe8b69e224f328bf71184809bfba0fc1c81c
tomasgvgt <tomasgvg.t@gmail.com>
2020-11-24 17:24:20 -0500

    find the lowest common ancestor of two nodes

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *mother, *father;
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+	mother = first->parent, father = second->parent;
+	if (first == father || !mother || (!mother->parent && father))
+		return (binary_trees_ancestor(first, father));
+	else if (mother == second || !father || (!father->parent && mother))
+		return (binary_trees_ancestor(mother, second));
+	return (binary_trees_ancestor(mother, father));
+}
