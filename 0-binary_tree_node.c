dfea85d8261bf532d3adbb4a6a26ba4855968a47
Deyber2000 <garzondeiber6@gmail.com>
2020-11-26 16:12:18 -0500

    fixed betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,6 +10,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
+
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (NULL);
@@ -18,5 +19,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new->left = NULL;
 	new->right = NULL;
 
-	return (new);	
+	return (new);
 }
