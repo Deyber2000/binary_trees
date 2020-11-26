c5f7ab71c02204b73aa9de6e2069d9d761189e47
Deyber2000 <garzondeiber6@gmail.com>
2020-11-26 17:43:22 -0500

    description

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binery_tree_is_root - checks if a node is the root.
+ * binary_tree_is_root - checks if a node is the root.
  * @node: node.
  * Return: 1 if root, 0 if not.
  */
@@ -10,5 +10,5 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node != NULL && node->parent == NULL)
 		return (1);
-	return (0);	
+	return (0);
 }
