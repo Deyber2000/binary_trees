70c987e3e55d21b28ae189f3a3874cd2178e3cbd
Deyber2000 <garzondeiber6@gmail.com>
2020-11-25 21:22:08 -0500

    rotate binary tree to the left

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *pivot, *tmp;
+
+	if (!tree || !tree->right)
+		return (NULL);
+	pivot = tree->right;
+	tmp = pivot->left;
+	pivot->left = tree;
+	tree->right = tmp;
+	if (tmp)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;	
+	}
+	return (pivot);		
+}
