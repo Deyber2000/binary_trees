8c9ed246ace79a802197331c887d5878fd661ffc
Deyber2000 <garzondeiber6@gmail.com>
2020-11-26 17:25:22 -0500

    value parameter for betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,25 +3,28 @@
 /**
  * binary_tree_insert_left -  inserts a node as the left-child of another node.
  * @parent: nodes parent.
- * @valiue: nodes value.
- * Return: eturn a pointer to the created node, or NULL on failure or if parent is NULL.
+ * @value: nodes value.
+ * Return: eturn a pointer to the created node,
+ *  or NULL on failure or if parent is NULL.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
+
 	if (parent == NULL)
 		return (NULL);
 	new = binary_tree_node(parent, value);
 	if (!new)
 		return (NULL);
-	/*OJO WE have to make 3 connections if !parent->left, and 4 connections if parent->left*/
+	/*OJO WE have to make 3 connections if !parent->left*/
+	/*and 4 connections if parent->left*/
 	if (parent->left != NULL)
 	{
-		new->left = parent->left; 
+		new->left = parent->left;
 		parent->left->parent = new; /*conect new->parent to root*/
 	}
 	/* If there's not a node in parent->left position*/
 	parent->left = new;
-	return (new);		
+	return (new);
 }
