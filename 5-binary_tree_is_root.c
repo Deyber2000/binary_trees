0f85245e6c2f2c59171410c99006724b0391311e
tomasgvgt <tomasgvg.t@gmail.com>
2020-11-26 14:49:20 -0500

    Documentation for 0-5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binery_tree_is_root - checks if a node is the root.
+ * @node: node.
+ * Return: 1 if root, 0 if not.
+ */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node != NULL && node->parent == NULL)
