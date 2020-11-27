e8011e3e9b79af035d9280d9c083c17f4bbc75df
Deyber2000 <garzondeiber6@gmail.com>
2020-11-26 22:22:47 -0500

    space between two lines in comment

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -16,7 +16,7 @@ int binary_tree_is_bst_aux(const binary_tree_t *tree, int lowest, int highest)
 		if (tree->n < lowest || tree->n > highest)
 			return (0);
 		return (binary_tree_is_bst_aux(tree->left, lowest, tree->n - 1) &&
-			binary_tree_is_bst_aux(tree->right, tree->n + 1, highest));	
+			binary_tree_is_bst_aux(tree->right, tree->n + 1, highest));
 	}
 	return (1);
 }
@@ -24,6 +24,7 @@ int binary_tree_is_bst_aux(const binary_tree_t *tree, int lowest, int highest)
 /**
  * binary_tree_is_bst - CHecks if bt is BST.
  * @tree: root.
+ *
  * Return: True or False.
  */
 
@@ -31,5 +32,5 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (binary_tree_is_bst_aux(tree, INT_MIN, INT_MAX));	
+	return (binary_tree_is_bst_aux(tree, INT_MIN, INT_MAX));
 }
