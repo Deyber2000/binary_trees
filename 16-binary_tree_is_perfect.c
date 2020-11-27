b79463cd1cbcf77fc35896ee241c44a04db89a6e
Deyber2000 <garzondeiber6@gmail.com>
2020-11-26 20:59:03 -0500

    improving betty style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,6 +9,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l_height = 0, r_height = 0;
+
 	if (tree)
 	{
 
@@ -43,10 +44,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (!(node->left) && !(node->right))
 		return (1);
 	return (0);
-
-	/*if(node == NULL || node->left != NULL || node->right != NULL)*/
-	/*return (0)*/
-	/*return (1)*/
 }
 
 /**
@@ -65,7 +62,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 	{
-		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		if (binary_tree_is_perfect(tree->left)
+			&& binary_tree_is_perfect(tree->right))
 			return (1);
 	}
 	return (0);
