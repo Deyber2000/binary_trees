26e40c828722c933f766a1adbae71194c10f587e
tomasgvgt <tomasgvg.t@gmail.com>
2020-11-27 08:58:45 -0500

    14 clarifying comments

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,13 +5,14 @@
  * @tree: root.
  * Return: height plus 1.
  */
-
 size_t binary_tree_height_plus_one(const binary_tree_t *tree)
 {
 	size_t l_height = 0, r_height = 0;
 
 	if (tree)
 	{
+		/*Add on to height even in laste leaf*/
+		/*but only keep recurring if there is a node below*/
 		l_height = tree->left ? 1 + binary_tree_height_plus_one(tree->left) : 1;
 		r_height = tree->right ? 1 + binary_tree_height_plus_one(tree->right) : 1;
 		return ((l_height > r_height) ? l_height : r_height);
@@ -25,7 +26,6 @@ size_t binary_tree_height_plus_one(const binary_tree_t *tree)
  * @tree: root.
  * Return: balance factor.
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
